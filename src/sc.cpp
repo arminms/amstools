0b67ba0df5ae7ff90fb037ce5f6d8f6a89871648
Armin Sobhani <asobhani@sharcnet.ca>
2022-05-06 10:00:01 -0400

    Add total in file column for output

--- src/sc.cpp
+++ src/sc.cpp
@@ -107,7 +107,10 @@ int main(int argc, char* argv[])
                 if (0 == result.count("seqs")
                 &&  0 == result.count("bps")
                 &&  0 == result.count("max-seq-length") )
-                    std::cout << seqsn << '\t' << bpsn << '\t' << file << std::endl;
+                    std::cout << seqsn << '\t'
+                              << bpsn  << '\t'
+                              << file
+                              << std::endl;
                 else
                 {
                     if (result.count("seqs"))
@@ -124,7 +127,10 @@ int main(int argc, char* argv[])
                 if (0 == result.count("seqs")
                 &&  0 == result.count("bps")
                 &&  0 == result.count("max-seq-length") )
-                    std::cout << seqsn_total << '\t' << bpsn_total << std::endl;
+                    std::cout << seqsn_total << '\t'
+                              << bpsn_total
+                              << "\ttotal"
+                              << std::endl;
                 else
                 {
                     if (result.count("seqs"))
@@ -133,7 +139,7 @@ int main(int argc, char* argv[])
                         std::cout << bpsn_total << '\t';
                     if (result.count("max-seq-length"))
                         std::cout << seqmax_total << '\t';
-                    std::cout << std::endl;
+                    std::cout << "total" << std::endl;
                 }
             }
         }
